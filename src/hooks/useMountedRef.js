f6ccc634a178eb9cec9d18877ee5fb8c93c09dd8
nate2211 <37088096+nate2211@users.noreply.github.com>
2021-11-28 22:08:47 -0600

    Add files via upload

---
+++ src/hooks/useMountedRef.js
@@ -0,0 +1,10 @@
+
+import {useRef, useEffect} from "react";
+export function useMountedRef() {
+    const mounted = useRef(false);
+    useEffect(() => {
+        mounted.current = true;
+        return () => (mounted.current = false)
+    })
+    return mounted;
+}
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+export const embed = "/embed";
+export const home = "/";
+export const upload = "/upload";
+export const signin = "/signin";
+export const signup = "/signup";
+export const forget = "/forget";
+export const account = "/account";
+export const signout = "/signout";
