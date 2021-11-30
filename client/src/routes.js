26eeca3502165d4e777d2211c3de865fd8d2aeba
nate2211 <37088096+nate2211@users.noreply.github.com>
2021-11-30 13:03:18 -0600

    Delete client directory

--- client/src/routes.js
+++
@@ -1,8 +0,0 @@
-export const embed = "/embed";
-export const home = "/";
-export const upload = "/upload";
-export const signin = "/signin";
-export const signup = "/signup";
-export const forget = "/forget";
-export const account = "/account";
-export const signout = "/signout";
